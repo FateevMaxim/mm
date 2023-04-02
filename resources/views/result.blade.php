04712ec2c11380adf049371028d59e89e51773d9
FateevMaxim <fateev.maxim@gmail.com>
2023-04-02 23:45:44 +0600

    updates

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -110,9 +110,7 @@
             </div>
                 <!-- Required chart.js -->
                 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
-                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
                 <script type="text/javascript">
 
